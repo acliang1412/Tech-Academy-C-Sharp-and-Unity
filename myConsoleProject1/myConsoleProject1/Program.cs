09300384746b2795b719a324667960440e11f1fd
acliang1412 <acliang7229@gmail.com>
2026-03-11 20:36:38 -0500

    Add files via upload
    Added files from TechAcademy Daily report Assisngment, unable to upload to github through Visual Studio Insiders

---
+++ myConsoleProject1/myConsoleProject1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            // This is a simple program 
+            //Console.WriteLine("What is your name?");
+            // string yourName = Console.ReadLine();
+            // Console.WriteLine("Hello " + yourName + ", welcome to C# programming!");
+            // Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNum = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNum);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+            
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            String myName = "Drew";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+            Console.WriteLine(rainingStatus);
+            Console.WriteLine(moneyInBank);
+        }
+    }
+}
