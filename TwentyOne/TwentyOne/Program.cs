ae23fedc7da77952191f71b59ba79f4518a29456
acliang1412 <acliang7229@gmail.com>
2026-03-14 18:25:56 -0500

    Add files via upload
    C# of making a deck of cards

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+            Console.ReadLine();
+
+        }
+    }
+}
